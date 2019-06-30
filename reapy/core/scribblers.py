ac1f2b0b9af9161b599d8a216bcc3b0ad6cf8b89
xXxRisingTidexXx <danylopetrik@gmail.com>
2019-07-01 02:32:35 +0300

    Documentating is started

--- reapy/core/scribblers.py
+++ reapy/core/scribblers.py
@@ -1,3 +1,12 @@
+"""
+This module describes *scribblers* - vigilant *reapy*'s statisticians
+
+Generally, each worker needs an auxiliary entity whose main purpose is to
+write all worker's numeric achievements. :class:`core.scribblers.Scribbler`
+and its successors are *.csv* file writers. This text format is very simple
+and suitable for table comprehensions, that's why *scribblers* dump all their
+reports into comma-separated-values files.
+"""
 from datetime import datetime
 from asyncio import Lock
 from csv import DictWriter
@@ -6,10 +15,24 @@ from . import BASE_DIR
 
 
 class Scribbler:
+    """
+    The parent of the statisticians' hierarchy
+
+    Rewrites all important worker's shapes into appropriate .csv file.
+    Shapes' set and their defaults depend on the worker. Each *reapy*'s
+    tact requires worker to dump its results, that's why each tact
+    produces 1 row in the 'scribble' file. Basically, any scribble's line
+    contains a few numeric params and the record's date&time.
+    """
     _fields = None
     _defaults = None
 
     def __init__(self, scribble_path):
+        """
+        Defines target file's path and fills the default data
+
+        :param scribble_path: scribble file's relative path
+        """
         self._scribble_path = join(BASE_DIR, scribble_path)
         self._row = {
             self._fields[i]: self._defaults[i]
@@ -18,21 +41,40 @@ class Scribbler:
         self._lock = Lock()
 
     def scribble_header(self):
+        """
+        Defines .csv headers and creates the scribble's file if it's absent
+        """
         if not exists(self._scribble_path):
             with open(self._scribble_path, 'w+') as stream:
                 DictWriter(stream, fieldnames=self._fields).writeheader()
 
     async def add(self, field, value=1):
+        """
+        Asynchronously increases the filed's value
+
+        :param field: numeric field to be increased
+        :param value: increasing value
+        """
         async with self._lock:
             self._row[field] += value
 
     def scribble_row(self):
+        """
+        Rewrites another line to the scribble's file, pointing
+        the date&time of the record
+        """
         self._row['written'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         with open(self._scribble_path, 'a') as stream:
             DictWriter(stream, fieldnames=self._fields).writerow(self._row)
 
 
 class ReaperScribbler(Scribbler):
+    """
+    This scribbler is specialized on the :class:`core.reapers.Reaper`'s
+    statistics. Generally, reapers add new and update existing data,
+    that's why their reports contain many shapes concernedly
+    inserted/duplicated/invalid data.
+    """
     _fields = (
         'inserted', 'updated', 'duplicated', 'unlocated',
         'unresponded', 'invalidated', 'unparsed', 'written'
@@ -41,5 +83,10 @@ class ReaperScribbler(Scribbler):
 
 
 class SweeperScribbler(Scribbler):
+    """
+    This scribbler is specialized on the :class:`core.sweepers.Sweeper`'s
+    statistics. Mainly, sweepers delete junk and expired data so that their
+    scribbles contain mainly data concernedly deletions.
+    """
     _fields = ('deleted', 'unresponded', 'written')
     _defaults = (0, 0, None)
--- reapy/core/utils.py
+++ reapy/core/utils.py
@@ -1,3 +1,6 @@
+"""
+Basic helping tools and instruments
+"""
 import aiofiles
 from decimal import Decimal, ROUND_HALF_EVEN
 from json import loads
@@ -9,37 +12,98 @@ from . import BASE_DIR
 
 
 def snake_case(s):
+    """
+    Converters string in CamelCase into snake_case
+
+    >>> snake_case('SpringBootBeanPostProcessor')
+    'spring_boot_bean_post_processor'
+    >>> snake_case('URL')
+    'url'
+
+    :param s: a string in CamelCase
+    :return: a string in snake_case
+    """
     return sub(r'([a-z])([A-Z])', r'\1_\2', s).lower()
 
 
 def decimalize(number, exp=Decimal('.001'), rounding=ROUND_HALF_EVEN):
+    """
+    Rounds the input float number
+
+    >>> decimalize('23.4353')
+    Decimal('23.435')
+    >>> decimalize(0.0019)
+    Decimal('0.002')
+
+    :param number: float number to be rounded
+    :param exp: rounding tolerance
+    :param rounding: rounding type
+    :return: rounded :class:`decimal.Decimal` value
+    """
     return Decimal(number).quantize(exp, rounding=rounding)
 
 
 async def map_filter(iterable, mapper, predicate=(lambda x: x is not None)):
+    """
+    Asynchronously converts and then filters the input sequence
+
+    :param iterable: the target sequence
+    :param mapper: coroutine-converter
+    :param predicate: synchronous filtering function
+    :return: mapped and filtered iterable
+    """
     return filter(predicate, await gather(*map(mapper, iterable)))
 
 
 def makedir(path):
+    """
+    Creates the folder by the provided relative path if it doesn't exist
+
+    :param path: folder's relative path concernedly the project's root
+    """
     abs_path = join(BASE_DIR, path)
     if not exists(abs_path):
         mkdir(abs_path)
 
 
 def exist(path):
+    """
+    Checks the existence of the provided directory or file
+
+    :param path: file's relative path concernedly the project's root
+    :return: file's existence
+    """
     return exists(join(BASE_DIR, path))
 
 
 def json(path):
+    """
+    Converts the provided .json file into Python objects
+
+    :param path: file's relative path concernedly the project's root
+    :return: file's content
+    """
     with open(join(BASE_DIR, path)) as stream:
         return loads(stream.read())
 
 
 async def load(path):
+    """
+    Asynchronous file loader
+
+    :param path: file's relative path concernedly the project's root
+    :return: file's content
+    """
     async with aiofiles.open(join(BASE_DIR, path)) as stream:
         return await stream.read()
 
 
 async def dump(path, data):
+    """
+    Asynchronous file dumper
+
+    :param path: file's relative path concernedly the project's root
+    :param data: string data to be written
+    """
     async with aiofiles.open(join(BASE_DIR, path), 'w+') as stream:
         await stream.write(data)
