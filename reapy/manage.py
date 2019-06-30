550aa1c78da40643ba93492bd8edefc3769c4acd
xXxRisingTidexXx <danylopetrik@gmail.com>
2019-06-30 03:32:17 +0300

    Documentating is started

--- reapy/manage.py
+++ reapy/manage.py
@@ -1,4 +1,22 @@
 #!/bin/env python3
+"""
+The entry point of each *reapy* script.
+
+The general purpose of *reapy* is data processing; the main working units are
+the subclasses of :class:`core.workers.Worker`. Each launch of the current file
+is a single tact of the workflow. Dependently on their specialization, workers
+modify the common database. That's why the most suitable workers' usage is via
+some task scheduler, like `cron <https://www.ostechnix.com/a-beginners-guide-to-cron-jobs/>`_.
+What's more, there's a module with the 'python-crontab' configuration - :mod:`schedule`.
+
+As it was mentioned, this file is for a single run of the *reapy*'s script. In
+console it looks like:
+```
+$ python manage.py <worker_name>
+```
+The full list of workers can be found on top of :mod:`core.workers`. Inappropriate
+worker's name causes error.
+"""
 from sys import argv
 from importlib import import_module
 
--- reapy/schedule.py
+++ reapy/schedule.py
@@ -1,4 +1,10 @@
 #!/bin/env python3
+"""
+*reapy*'s worker manager
+
+This simple script runs a set of workers in parallel via *cron*. Configure
+the timings and the worker list if you need.
+"""
 from crontab import CronTab
 from core import USER, BASE_DIR, INTERPRETER_PATH
 from os.path import join
@@ -6,15 +12,15 @@ from os.path import join
 CRON = CronTab(user=USER)
 
 
-def run_worker(worker, **kwargs):
+def __run_worker(worker, **kwargs):
     job = CRON.new(f'{INTERPRETER_PATH} {join(BASE_DIR, "manage.py")} {worker}')
     job.minute.on(*kwargs['minutes'])
     job.hour.on(*kwargs['hours'])
     return job
 
 
-run_worker('OlxFlatReaper', minutes=(0, 15, 30, 45), hours=(23, 0, 1, 2, 3, 4))
-run_worker('DomRiaFlatReaper', minutes=(8, 23, 38, 53), hours=(23, 0, 1, 2, 3, 4))
-run_worker('OlxFlatSweeper', minutes=(40,), hours=(22,))
-run_worker('DomRiaFlatSweeper', minutes=(50,), hours=(22,))
+__run_worker('OlxFlatReaper', minutes=(0, 15, 30, 45), hours=(23, 0, 1, 2, 3, 4))
+__run_worker('DomRiaFlatReaper', minutes=(8, 23, 38, 53), hours=(23, 0, 1, 2, 3, 4))
+__run_worker('OlxFlatSweeper', minutes=(40,), hours=(22,))
+__run_worker('DomRiaFlatSweeper', minutes=(50,), hours=(22,))
 CRON.write()
