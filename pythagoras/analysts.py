86a9bfb19d7f2ec449ac2ec72d5ecde7a948189b
xXxRisingTidexXx <danylopetrik@gmail.com>
2019-07-12 00:38:15 +0300

    Hotfix

---
+++ pythagoras/analysts.py
@@ -0,0 +1,15 @@
+class Analyst:
+    def analyze(self):
+        pass
+
+
+class FlatAnalyst(Analyst):
+    pass
+
+
+class PrimaryHousingAnalyst(FlatAnalyst):
+    pass
+
+
+class SecondaryHousingAnalyst(FlatAnalyst):
+    pass
