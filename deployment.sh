0bf79fe0b7903b34444d9734dcc535287b7d123f
xXxRisingTidexXx <danylopetrik@gmail.com>
2019-09-06 19:22:18 +0300

    deployment is building

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,35 @@
 #!/usr/bin/env bash
 printf "\nDeployment started\n"
 
-printf "\nDeployment finished\n\n"
+git pull origin dev
+
+cd ./agony/
+
+# Creates agony's infrastructural folders
+DIR="${PWD}/static"
+[[ ! -d ${DIR} ]] && mkdir -p ${DIR}
+DIR="${PWD}/logs"
+[[ ! -d ${DIR} ]] && mkdir -p ${DIR}
+touch "${PWD}/logs/access.log"
+
+# Loads agony's dependencies
+if [[ ! -d "${PWD}/venv" ]]; then
+    pip install --upgrade pip
+    pip install virtualenv
+    virtualenv venv
+fi
+pip install -r requirements.txt
+source ./venv/bin/activate
+
+deactivate
+cd ../reapy/
+
+# Creates reapy's infrastructural folders
+DIR="${PWD}/logs"
+[[ ! -d ${DIR} ]] && mkdir -p ${DIR}
+DIR="${PWD}/scribbles"
+[[ ! -d ${DIR} ]] && mkdir -p ${DIR}
+
+cd ../
+
+printf "Deployment finished\n\n"
