1709c72cf6aefe7e5a1c27c406b6d70bb61f67da
xXxRisingTidexXx <danylopetrik@gmail.com>
2019-09-07 12:28:37 +0300

    Hotfix

--- deployment.sh
+++ deployment.sh
@@ -3,33 +3,47 @@ printf "\nDeployment started\n"
 
 git pull origin dev
 
-cd ./agony/
+# Edit YamJam config
 
-# Creates agony's infrastructural folders
+# Create agony's infrastructural folders
+cd ./agony/
 DIR="${PWD}/static"
 [[ ! -d ${DIR} ]] && mkdir -p ${DIR}
 DIR="${PWD}/logs"
 [[ ! -d ${DIR} ]] && mkdir -p ${DIR}
 touch "${PWD}/logs/access.log"
 
-# Loads agony's dependencies
+# Load agony's dependencies
 if [[ ! -d "${PWD}/venv" ]]; then
     pip install --upgrade pip
     pip install virtualenv
     virtualenv venv
 fi
 pip install -r requirements.txt
+STATUS=$?;
+if [[ ${STATUS} != 0 ]]; then
+    exit ${STATUS};
+fi
 source ./venv/bin/activate
 
-deactivate
-cd ../reapy/
+# Migrate the databases
 
-# Creates reapy's infrastructural folders
+# Test the API
+./manage.py test
+./manage.py test --reverse
+
+# Collect static files
+
+# Finalize agony's configuration
+
+deactivate
+
+# Create reapy's infrastructural folders
+cd ../reapy/
 DIR="${PWD}/logs"
 [[ ! -d ${DIR} ]] && mkdir -p ${DIR}
 DIR="${PWD}/scribbles"
 [[ ! -d ${DIR} ]] && mkdir -p ${DIR}
-
 cd ../
 
 printf "Deployment finished\n\n"
